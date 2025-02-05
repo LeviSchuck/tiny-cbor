0f75f427bf6aac367e3f3561d55406da75290fa3
Levi Schuck <me@levischuck.com>
2025-02-04 20:38:04 -0600

    fix: Format

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,5 +1,8 @@
 const DECODER = new TextDecoder();
-const json = JSON.parse(DECODER.decode(Deno.readFileSync('deno.json')));
-const version = DECODER.decode(Deno.readFileSync('version.txt'));
+const json = JSON.parse(DECODER.decode(Deno.readFileSync("deno.json")));
+const version = DECODER.decode(Deno.readFileSync("version.txt"));
 json.version = version;
-Deno.writeFileSync('deno.json', new TextEncoder().encode(JSON.stringify(json, null, 2)));
+Deno.writeFileSync(
+  "deno.json",
+  new TextEncoder().encode(JSON.stringify(json, null, 2)),
+);
