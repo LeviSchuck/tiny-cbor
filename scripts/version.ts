32e4d67d210083f7de21e4d9297f3c8253ca3660
Levi Schuck <me@levischuck.com>
2025-02-04 21:49:30 -0600

    chore: Remove \n from version

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,8 +1,8 @@
 const DECODER = new TextDecoder();
 const json = JSON.parse(DECODER.decode(Deno.readFileSync("deno.json")));
 const version = DECODER.decode(Deno.readFileSync("version.txt"));
-json.version = version;
+json.version = version.trim();
 Deno.writeFileSync(
   "deno.json",
-  new TextEncoder().encode(JSON.stringify(json, null, 2)),
+  new TextEncoder().encode(JSON.stringify(json, null, 2) + "\n"),
 );
