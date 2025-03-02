cf5a3b813ade5357026336f98706c42c5695ed10
LeviSchuck <me@levischuck.com>
2025-03-02 15:22:48 -0600

    export shorthand

--- index.ts
+++ index.ts
@@ -4,4 +4,8 @@ export {
   decodePartialCBOR,
   encodeCBOR,
 } from "./cbor/cbor.ts";
+export {
+  cs,
+} from './cbor/cbor_schema.ts'
 export type { CBORType } from "./cbor/cbor.ts";
+export type { FieldDefinition, CBORSchema } from "./cbor/cbor_schema.ts";
